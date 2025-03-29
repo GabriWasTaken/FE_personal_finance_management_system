984774caad7ed302337aa0dba49a665350699dc4
gabriele <gabrieletorta@gmail.com>
2025-03-29 21:03:29 +0100

    feature: refresh token

--- src/hooks/useErrorManager.ts
+++ src/hooks/useErrorManager.ts
@@ -1,12 +1,20 @@
 import { useLogto } from "@logto/react";
 export function useErrorManager() {
 
-  const {signOut} = useLogto();
+  const {getIdToken, getAccessToken} = useLogto();
 
   const handleError = ({ error }: { error: {status: number} }) => {
     if (error.status === 401) {
       localStorage.removeItem('token');
-      signOut();
+      localStorage.removeItem('accessToken');
+
+      async function retrieveLogtoInfo() {
+        const accessToken = await getAccessToken();
+        const token = await getIdToken();
+        if (token) localStorage.setItem("token", token);
+        if (accessToken) localStorage.setItem("accessToken", accessToken);
+      }
+      retrieveLogtoInfo();
     }
   }
 
