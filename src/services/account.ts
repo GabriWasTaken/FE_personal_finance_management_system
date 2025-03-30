8d09f5b59a18d11b5e22a66a238e4ec09b5c746a
gabriele <gabrieletorta@gmail.com>
2025-03-30 22:54:39 +0200

    feature: add auth on addAccount API

--- src/services/account.ts
+++ src/services/account.ts
@@ -1,5 +1,9 @@
 export const addAccount = async ({name, handleError}: {name: string, handleError: ({error}: {error: {status: number}}) => void}) => {
-  const response = await fetch(import.meta.env.VITE_BASE_URL + '/accounts', { method: 'POST', body: JSON.stringify({ name }) })
+  const response = await fetch(import.meta.env.VITE_BASE_URL + '/accounts', { 
+      method: 'POST',
+      body: JSON.stringify({ name }),
+      headers: { 'Content-Type': 'application/json', 'Authorization': `Bearer ${localStorage.getItem('token')}`} 
+    });
   if (!response.ok) {
     handleError({ error: response });
     throw new Error('Network response was not ok')
