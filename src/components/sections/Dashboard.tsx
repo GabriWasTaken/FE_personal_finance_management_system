5809faf9f4c6e481e2175ecd9d6306c6145f7baa
gabriele <gabrieletorta@gmail.com>
2025-04-09 23:25:37 +0200

    feat: some aggregate data in dashboard

--- src/components/sections/Dashboard.tsx
+++ src/components/sections/Dashboard.tsx
@@ -2,9 +2,38 @@ import { QueryObserverSuccessResult, QueryObserverPlaceholderResult } from '@tan
 import { PaginationState } from '@tanstack/react-table';
 import React from 'react';
 
-function Dashboard( {dataQuery, pagination, setPagination}: {dataQuery: QueryObserverSuccessResult<unknown, Error> | QueryObserverPlaceholderResult<unknown, Error>, pagination: PaginationState, setPagination: React.Dispatch<React.SetStateAction<PaginationState>>} ) {
+type DashboardSuccessAPIReturn = {
+  allTime: {
+    total_income: number,
+    total_expense: number
+  },
+  YTD: {
+    total_income: number,
+    total_expense: number
+  },
+  prevYear: {
+    total_income: number,
+    total_expense: number
+  }
+
+}
+
+function Dashboard({ dataQuery, pagination, setPagination }: { dataQuery: QueryObserverSuccessResult<DashboardSuccessAPIReturn, Error> | QueryObserverPlaceholderResult<DashboardSuccessAPIReturn, Error>, pagination: PaginationState, setPagination: React.Dispatch<React.SetStateAction<PaginationState>> }) {
+  if (!dataQuery.data.allTime || !dataQuery.data.YTD || !dataQuery.data.prevYear) return <div>Loading...</div>;
   return (
-    <div>Dashboard</div>
+    <div>
+      Dashboard
+      {dataQuery.data &&
+        <div>
+          All time income: {dataQuery.data.allTime.total_income} <br />
+          All time expenses: {dataQuery.data.allTime.total_expense} <br />
+          YTD income: {dataQuery.data.YTD.total_income} <br />
+          YTD expenses: {dataQuery.data.YTD.total_expense} <br />
+          prev year income: {dataQuery.data.prevYear.total_income} <br />
+          prev year expenses: {dataQuery.data.prevYear.total_expense}
+        </div>
+      }
+    </div>
   )
 }
 
