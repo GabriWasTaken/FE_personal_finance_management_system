4dce232c49f9cf07ddcb67515513119c0fcc5a00
gabriele <gabrieletorta@gmail.com>
2025-05-20 22:58:33 +0200

    bugfix: access token

--- src/hooks/useErrorManager.ts
+++ src/hooks/useErrorManager.ts
@@ -3,7 +3,7 @@ import { toast } from "sonner"
 
 export function useErrorManager() {
 
-  const {getIdToken, getAccessToken} = useLogto();
+  const {getIdToken, getAccessToken, signOut} = useLogto();
 
   const handleError = ({ error }: { error: {status: number} }) => {
     if (error.status === 401) {
@@ -14,7 +14,12 @@ export function useErrorManager() {
         const accessToken = await getAccessToken();
         const token = await getIdToken();
         if (token) localStorage.setItem("token", token);
-        if (accessToken) localStorage.setItem("accessToken", accessToken);
+        if (accessToken) {
+          localStorage.setItem("accessToken", accessToken);
+        } else {
+          localStorage.removeItem("isAuthenticated");
+          signOut();
+        }
       }
       retrieveLogtoInfo();
     }
