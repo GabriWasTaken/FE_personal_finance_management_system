890589f42f4ca266f7df5479f29442dd63d5b531
gabriele <gabrieletorta@gmail.com>
2025-05-22 23:59:35 +0200

    feat: delete account

--- src/components/sections/Accounts.tsx
+++ src/components/sections/Accounts.tsx
@@ -4,20 +4,24 @@ import TableServerSide from '../listComponents/TableServerSide';
 import { QueryObserverPlaceholderResult, QueryObserverSuccessResult, useMutation, useQueryClient } from '@tanstack/react-query';
 import { Button } from '../ui/button';
 import { useNavigate } from "react-router";
-import { addAccount } from '@/services/account';
+import { addAccount, deleteAccount } from '@/services/account';
 import { useErrorManager } from '@/hooks/useErrorManager';
 import i18next from 'i18next';
 import { toast } from "sonner"
+import { Trash2 } from 'lucide-react';
+import ConfirmDeleteModal from '../ui/confirmDeleteModal';
 
 function Accounts({ dataQuery, pagination, setPagination }: { dataQuery: QueryObserverSuccessResult<unknown, Error> | QueryObserverPlaceholderResult<unknown, Error>, pagination: PaginationState, setPagination: React.Dispatch<React.SetStateAction<PaginationState>> }) {
   type ColDef = {
-    id: string,
+    id: number,
     name: string,
     net_total: string
   }
   const navigate = useNavigate();
   const queryClient = useQueryClient();
   const handleError = useErrorManager();
+  const [idToDelete, setIdToDelete] = React.useState<number>();
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = React.useState(false);
 
   const mutation = useMutation({
     mutationKey: ['/accounts'],
@@ -32,6 +36,18 @@ function Accounts({ dataQuery, pagination, setPagination }: { dataQuery: QueryOb
     },
   })
 
+  const mutationDeleteFinancial = useMutation({
+    mutationKey: ['/accountsDelete'],
+    mutationFn: deleteAccount,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['/accounts'] });
+    },
+    onError: () => {
+      // Remove optimistic todo from the todos list
+      //TODO insert error msg
+    },
+  })
+
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     const form = event.target as HTMLFormElement;
@@ -39,6 +55,14 @@ function Accounts({ dataQuery, pagination, setPagination }: { dataQuery: QueryOb
     const name = formdata.get('name') as string;
     mutation.mutate({name, handleError});
     form.reset();
+  }
+
+  const handleDeleteFinancial = () => {
+    if (idToDelete) {
+      mutationDeleteFinancial.mutate({ id: idToDelete, handleError })
+      setIdToDelete(undefined);
+      setIsDeleteModalOpen(false);
+    }
   }
 
   const columns = React.useMemo<ColumnDef<ColDef>[]>(
@@ -65,6 +89,20 @@ function Accounts({ dataQuery, pagination, setPagination }: { dataQuery: QueryOb
           })}>{row.original.net_total}</div>
         },
       },
+      {
+        accessorKey: "delete",
+        header: () => <div>Action</div>,
+        cell: ({ row }) => {
+          const handleDelete = () => {
+            console.log(row.original.id);
+            if (setIdToDelete && setIsDeleteModalOpen) {
+              setIdToDelete(row.original.id);
+              setIsDeleteModalOpen(true);
+            }
+          }
+          return <div className="font-medium"><Trash2 className="h-6 w-6 hover:cursor-pointer" onClick={handleDelete} /></div>;
+        },
+      },
     ],
     []
   )
@@ -75,6 +113,7 @@ function Accounts({ dataQuery, pagination, setPagination }: { dataQuery: QueryOb
         <Button type='submit' variant='secondary'>{i18next.t('Common.add')}</Button>
       </form>
       <TableServerSide columns={columns} dataQuery={dataQuery} pagination={pagination} setPagination={setPagination} />
+      <ConfirmDeleteModal isOpen={isDeleteModalOpen} setIsOpen={setIsDeleteModalOpen} handleDelete={() => handleDeleteFinancial()} />
     </>
   )
 }
